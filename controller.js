7ad431cd37aadcc972e9086c09f4ed5171b67da8
heryalfian_ <heryalf@gmail.com>
2021-04-27 06:13:27 +0800

    berhasil menampilkan data produk

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ let connection = require('./koneksi');
 exports.index = (req, res) => {
     response.ok(`Aplikasi REST API Berjalan!`, res)
 };
+
+//menampilkan data produk
+exports.tampilproduk = (req, res) => {
+    connection.query(`SELECT * FROM Produk`, (error, rows, field) => {
+        if (error) {
+            connection.log(error)
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
